f3f0b504c3eb28c65b4c4bfb390fac85b02b7ec6
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-05-11 12:50:38 +0000

    get動く状態

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, Responder, HttpResponse, HttpServer, App};
+
+#[get("/todos")]
+async fn get_todos() -> impl Responder {
+    HttpResponse::Ok().body("/todos")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new( || {
+        App::new()
+            .service(get_todos)
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
