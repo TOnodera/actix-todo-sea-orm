c1590bf847b489f688a6abd0e4a1071deb8f3d1d
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-05-14 06:56:19 +0000

    ドキュメント通りのテストを実装

---
+++ app/tests/test.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+mod tests {
+    use actix_web::{get, test, web, App, HttpResponse, Responder};
+
+    #[get("/")]
+    async fn index() -> impl Responder {
+        HttpResponse::Ok().body("test")
+    }
+
+    #[actix_web::test]
+    async fn test_index_get() {
+        let app = test::init_service(App::new().route(
+            "/",
+            web::get().to(|| async {
+                return HttpResponse::Ok().body("test");
+            }),
+        ))
+        .await;
+
+        let request = test::TestRequest::default().to_request();
+        let response = test::call_service(&app, request).await;
+        assert!(response.status().is_success());
+    }
+}
